7b3f5d8151659fbccf3ce9c0b7e28e4a950e270e
chakri68 <chakridevireddy69@gmail.com>
2023-06-21 16:41:16 +0530

    🎉 Added required webpack config and packages

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import "./styles/main.css";
+import "./styles/sass/app.scss";
+
+console.log("index.js connected!");
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+
+module.exports = {
+  mode: "none",
+  entry: "./src/index.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js",
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    hot: true,
+  },
+  resolve: {
+    extensions: ["*", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(s(a|c)ss)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
